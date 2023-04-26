387e053b25c11e18092eeec88e5f9e9b89647bd9
bberioso01 <105355071+bberioso01@users.noreply.github.com>
2023-04-26 22:27:18 +0800

    Added form validation in Login

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -3,32 +3,37 @@ const btnSignIn = document.querySelector("#signIn");
 
 btnSignIn.addEventListener("click", async (event) => {
   event.preventDefault();
-  const formdata = new FormData(form);
-
-  try {
-    const api = await fetch("/authlogin", {
-      method: "POST",
-      body: formdata,
-    });
-    const res = await api.json();
-    if (res.isSuccess) {
-      Swal.fire({
-        icon: "success",
-        title: "Login Success",
-        confirmButtonText: "Ok",
-        allowOutsideClick: () => (window.location.href = "/"),
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.location.href = "/";
-        }
+  if (!form.checkValidity()) {
+    form.classList.add("was-validated");
+  } else {
+    const formdata = new FormData(form);
+    try {
+      const api = await fetch("/authlogin", {
+        method: "POST",
+        body: formdata,
       });
-    } else {
-      Swal.fire({
-        icon: "error",
-        title: "Login Failed",
-      });
+      const res = await api.json();
+      if (res.isSuccess) {
+        Swal.fire({
+          icon: "success",
+          title: "Login Success",
+          confirmButtonText: "Ok",
+          allowOutsideClick: () => (window.location.href = "/"),
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = "/";
+          }
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Login Failed",
+        });
+        form.reset();
+        form.classList.remove("was-validated");
+      }
+    } catch (err) {
+      console.log(err);
     }
-  } catch (err) {
-    console.log(err);
   }
 });
